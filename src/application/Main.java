86a598ff3573ba35028b1960c202cb3611ebd195
unknown <kelvindisi@yahoo.com>
2020-06-10 13:27:20 +0300

    Event handling
    Event handling after button click

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,11 +3,14 @@ package application;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.event.*;
 
-public class Main extends Application{
-
+public class Main extends Application implements EventHandler{
+	private Button btn;
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
@@ -16,12 +19,19 @@ public class Main extends Application{
 	public void start(Stage primaryStage) throws Exception {
 		primaryStage.setTitle("JavaFx");
 		StackPane layout = new StackPane();
-		Button btn = new Button("Click Me!");
+		btn = new Button("Click Me!");
+		btn.setOnAction(this);
 		layout.getChildren().add(btn);
 		Scene scene = new Scene(layout, 400, 400);
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
-	
+
+	@Override
+	public void handle(Event event) {
+		if (event.getSource() == btn) {
+			System.out.println("Clicked Me");
+		}
+	}
 
 }
